ccb586719a40193af237608294f81c9733c084cb
dongwangcheng <1540592414@qq.com>
2015-07-23 22:29:13 +0800

    增加注册时验证手机或邮箱

--- src/com/tt/potomemo/modules/user/service/UserService.java
+++ src/com/tt/potomemo/modules/user/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.tt.potomemo.common.persistence.Page;
 import com.tt.potomemo.common.service.CrudService;
+import com.tt.potomemo.common.utils.PhoneAndEmailUtil;
 import com.tt.potomemo.modules.user.dao.UserDao;
 import com.tt.potomemo.modules.user.entity.User;
 
@@ -34,6 +35,23 @@ public class UserService extends CrudService<UserDao, User> {
 	@Transactional(readOnly = false)
 	public void save(User user) {
 		super.save(user);
+	}
+	
+	@Transactional(readOnly = false)
+	public boolean register(User user){
+		boolean state=false;
+		boolean mobileNO = PhoneAndEmailUtil.isMobileNO(user.getPhone());
+		boolean email = PhoneAndEmailUtil.isEmail(user.getPhone());
+		if(mobileNO){
+			super.save(user);
+			state = true;
+		}else if(email){
+			user.setEmail(user.getPhone());
+			user.setPhone(null);
+			super.save(user);
+			state = true;
+		}
+		return state;
 	}
 	
 	@Transactional(readOnly = false)
